57cd1112e8e22ad1d3a8a2594aff31ca6691a272
rqfvitor@gmail.com <rqfvitor@gmail.com>
2023-10-12 16:04:26 -0300

    refactor: move page related content to a pages subfolder

--- SGFI/apps/core/views.py
+++ SGFI/apps/core/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'apps/core/index.html')
+    return render(request, 'apps/core/pages/index.html')
+
+def form(request):
+    return render(request, 'apps/core/pages/form.html')
