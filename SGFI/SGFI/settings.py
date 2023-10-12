b3afb513b4e04221ef632366e44bce70cf06e0e2
rqfvitor@gmail.com <rqfvitor@gmail.com>
2023-10-12 12:40:17 -0300

    feat: add initial template structure

--- SGFI/SGFI/settings.py
+++ SGFI/SGFI/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'SGFI.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
