ae069a28287834b76eba7ddd6512c526638a59e8
Vitor Rafael <95248045+vitorqf@users.noreply.github.com>
2023-10-25 08:51:19 -0300

    History tweaking (#10)
    * fix: update scroll issues; feat: add counter on kanban header
    
    * feat: add history table

--- SGFI/apps/core/views.py
+++ SGFI/apps/core/views.py
@@ -1,16 +1,8 @@
 from django.shortcuts import render
 
 
-# Create your views here.
-def index(request):
-    return render(request, 'apps/core/pages/index.html')
-
-def form(request):
-    return render(request, 'apps/core/pages/form.html')
-
-def dashboard(request):
-    role = 'admin'
-    data = [
+data = {
+    "prints": [
         {
             'id': 1,
             'requested_at': '16/02/2023',
@@ -40,6 +32,7 @@ def dashboard(request):
             'withdraw_date': '19/02/2023',
             'withdraw_time': '11:45',
             'status': 'withdrawn',
+            'withdrawn_at': '26/03/2023'
         },
         {
             'id': 4,
@@ -70,6 +63,7 @@ def dashboard(request):
             'withdraw_date': '22/02/2023',
             'withdraw_time': '09:30',
             'status': 'withdrawn',
+            'withdrawn_at': '26/03/2023'
         },
         {
             'id': 7,
@@ -100,86 +94,95 @@ def dashboard(request):
             'withdraw_date': '25/02/2023',
             'withdraw_time': '10:00',
             'status': 'withdrawn',
+            'withdrawn_at': '26/03/2023'
         },
-    ]
-    return render(request, 'apps/core/pages/dashboard.html', { 'role': role, 'requests': data })
+        {
+            'id': 10,
+            'requested_at': '24/02/2023',
+            'requested_by': 'Manuel Costa',
+            'attachment': 'Apresentações de Inglês',
+            'attachment_count': 6,
+            'withdraw_date': '25/02/2023',
+            'withdraw_time': '10:00',
+            'status': 'withdrawn',
+            'withdrawn_at': '26/03/2023'
+        },
+        {
+            'id': 11,
+            'requested_at': '24/02/2023',
+            'requested_by': 'Manuel Costa',
+            'attachment': 'Apresentações de Inglês',
+            'attachment_count': 12,
+            'withdraw_date': '25/02/2023',
+            'withdraw_time': '10:00',
+            'status': 'withdrawn',
+            'withdrawn_at': '26/03/2023'
+        },
+        {
+            'id': 13,
+            'requested_at': '24/02/2023',
+            'requested_by': 'Manuel Costa',
+            'attachment': 'Apresentações de Inglês',
+            'attachment_count': 6,
+            'withdraw_date': '25/02/2023',
+            'withdraw_time': '10:00',
+            'status': 'withdrawn',
+            'withdrawn_at': '26/03/2023'
+        },
+        {
+            'id': 14,
+            'requested_at': '24/02/2023',
+            'requested_by': 'Manuel Costa',
+            'attachment': 'Apresentações de Inglês',
+            'attachment_count': 6,
+            'withdraw_date': '25/02/2023',
+            'withdraw_time': '10:00',
+            'status': 'withdrawn',
+            'withdrawn_at': '26/03/2023'
+        },
+        {
+            'id': 15,
+            'requested_at': '24/02/2023',
+            'requested_by': 'Manuel Costa',
+            'attachment': 'Apresentações de Inglês',
+            'attachment_count': 6,
+            'withdraw_date': '25/02/2023',
+            'withdraw_time': '10:00',
+            'status': 'withdrawn',
+            'withdrawn_at': '26/03/2023'
+        },
+        {
+            'id': 16,
+            'requested_at': '24/02/2023',
+            'requested_by': 'Manuel Costa',
+            'attachment': 'Apresentações de Inglês',
+            'attachment_count': 6,
+            'withdraw_date': '25/02/2023',
+            'withdraw_time': '10:00',
+            'status': 'withdrawn',
+            'withdrawn_at': '26/03/2023'
+        },
+    ],
+    "withdrawn_count": 9,
+    "pending_count": 3,
+    "printed_count": 3
+}
+
+# Create your views here.
+def index(request):
+    return render(request, 'apps/core/pages/index.html')
+
+def form(request):
+    return render(request, 'apps/core/pages/form.html')
+
+def dashboard(request):
+    role = 'admin'
+    context = data
+    
+    return render(request, 'apps/core/pages/dashboard.html', { 'role': role, 'requests': context })
 
 def history(request):
     role = 'admin'
-    data = [
-        {
-            'id': 1,
-            'requested_at': '16/02/2023',
-            'requested_by': 'John Doe Foo Bar',
-            'withdraw_date': '16/02/2023',
-            'content': 'Prova de Matemática, Prova de Química, Prova de Português',
-            'withdrawed': 'true'       
-        },
-        {
-            'id': 2,
-            'requested_at': '17/02/2023',
-            'requested_by': 'Alice Smith',
-            'withdraw_date': '18/02/2023',
-            'content': 'Prova de Matemática, Prova de Química, Prova de Português',
-            'withdrawed': 'true'      
-             
-        },
-        {
-            'id': 3,
-            'requested_at': '18/02/2023',
-            'requested_by': 'Bob Johnson',
-            'withdraw_date': '19/02/2023',
-            'content': 'Prova de Matemática, Prova de Química, Prova de Português',
-            'withdrawed': 'true'
-        },
-        {
-            'id': 4,
-            'requested_at': '19/02/2023',
-            'requested_by': 'Maria Silva',
-            'withdraw_date': '20/02/2023',
-            'content': 'Prova de Matemática, Prova de Química, Prova de Português',
-            'withdrawed': 'true'       
-        },
-        {
-            'id': 5,
-            'requested_at': '20/02/2023',
-            'requested_by': 'José Santos',
-            'withdraw_date': '21/02/2023',
-            'content': 'Prova de Matemática, Prova de Química, Prova de Português',
-            'withdrawed': 'false'       
-        },
-        {
-            'id': 6,
-            'requested_at': '21/02/2023',
-            'requested_by': 'Ana Rodrigues',
-            'withdraw_date': '22/02/2023',
-            'content': 'Prova de Matemática, Prova de Química, Prova de Português',
-            'withdrawed': 'false'
-        },
-        {
-            'id': 7,
-            'requested_at': '22/02/2023',
-            'requested_by': 'Ricardo Ferreira',
-            'withdraw_date': '23/02/2023',
-            'content': 'Prova de Matemática, Prova de Química, Prova de Português',
-            'withdrawed': 'true'       
-        },
-        {
-            'id': 8,
-            'requested_at': '23/02/2023',
-            'requested_by': 'Carla Lima',
-            'withdraw_date': '24/02/2023',
-            'content': 'Prova de Matemática, Prova de Química, Prova de Português',
-            'withdrawed': 'false'       
-        },
-        {
-            'id': 9,
-            'requested_at': '24/02/2023',
-            'requested_by': 'Manuel Costa',
-            'withdraw_date': '25/02/2023',
-            'content': 'Prova de Matemática, Prova de Química, Prova de Português',
-            'withdrawed': 'true'
-        }
-    ]
-       
-    return render(request, 'apps/core/pages/history.html', { 'role': role, 'requests': data })
+    context= data
+    
+    return render(request, 'apps/core/pages/history.html', { 'role': role, 'requests': context })
