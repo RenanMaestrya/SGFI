22485f60f9d8f6a425e6ffcc8092c89af1ebbfef
Renan Almeida <101940943+RenanMaestrya@users.noreply.github.com>
2023-10-25 08:08:04 -0300

    History screen (#8)
    * historico sendo feito
    
    * pulling develop
    
    * in progress card history
    
    ---------
    
    Co-authored-by: Vitor Rafael <95248045+vitorqf@users.noreply.github.com>

--- SGFI/apps/core/views.py
+++ SGFI/apps/core/views.py
@@ -105,4 +105,81 @@ def dashboard(request):
     return render(request, 'apps/core/pages/dashboard.html', { 'role': role, 'requests': data })
 
 def history(request):
-    return render(request, 'apps/core/pages/history.html')
+    role = 'admin'
+    data = [
+        {
+            'id': 1,
+            'requested_at': '16/02/2023',
+            'requested_by': 'John Doe Foo Bar',
+            'withdraw_date': '16/02/2023',
+            'content': 'Prova de Matemática, Prova de Química, Prova de Português',
+            'withdrawed': 'true'       
+        },
+        {
+            'id': 2,
+            'requested_at': '17/02/2023',
+            'requested_by': 'Alice Smith',
+            'withdraw_date': '18/02/2023',
+            'content': 'Prova de Matemática, Prova de Química, Prova de Português',
+            'withdrawed': 'true'      
+             
+        },
+        {
+            'id': 3,
+            'requested_at': '18/02/2023',
+            'requested_by': 'Bob Johnson',
+            'withdraw_date': '19/02/2023',
+            'content': 'Prova de Matemática, Prova de Química, Prova de Português',
+            'withdrawed': 'true'
+        },
+        {
+            'id': 4,
+            'requested_at': '19/02/2023',
+            'requested_by': 'Maria Silva',
+            'withdraw_date': '20/02/2023',
+            'content': 'Prova de Matemática, Prova de Química, Prova de Português',
+            'withdrawed': 'true'       
+        },
+        {
+            'id': 5,
+            'requested_at': '20/02/2023',
+            'requested_by': 'José Santos',
+            'withdraw_date': '21/02/2023',
+            'content': 'Prova de Matemática, Prova de Química, Prova de Português',
+            'withdrawed': 'false'       
+        },
+        {
+            'id': 6,
+            'requested_at': '21/02/2023',
+            'requested_by': 'Ana Rodrigues',
+            'withdraw_date': '22/02/2023',
+            'content': 'Prova de Matemática, Prova de Química, Prova de Português',
+            'withdrawed': 'false'
+        },
+        {
+            'id': 7,
+            'requested_at': '22/02/2023',
+            'requested_by': 'Ricardo Ferreira',
+            'withdraw_date': '23/02/2023',
+            'content': 'Prova de Matemática, Prova de Química, Prova de Português',
+            'withdrawed': 'true'       
+        },
+        {
+            'id': 8,
+            'requested_at': '23/02/2023',
+            'requested_by': 'Carla Lima',
+            'withdraw_date': '24/02/2023',
+            'content': 'Prova de Matemática, Prova de Química, Prova de Português',
+            'withdrawed': 'false'       
+        },
+        {
+            'id': 9,
+            'requested_at': '24/02/2023',
+            'requested_by': 'Manuel Costa',
+            'withdraw_date': '25/02/2023',
+            'content': 'Prova de Matemática, Prova de Química, Prova de Português',
+            'withdrawed': 'true'
+        }
+    ]
+       
+    return render(request, 'apps/core/pages/history.html', { 'role': role, 'requests': data })
