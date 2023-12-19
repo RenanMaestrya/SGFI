5233abb9a5fb9e591517c7cafec73bd051555323
Vitor Rafael <rqfvitor@gmail.com>
2023-12-19 10:45:01 -0300

    Add S3 Bucket, RDS Database, minor UI adjustments

--- project/apps/core/models.py
+++ project/apps/core/models.py
@@ -1,9 +1,17 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from main.settings import MAX_FILE_SIZE
+
 
 def get_file_path(_instance, filename):
     return f"files/{filename}"
+
+def file_size(value):
+    limit = MAX_FILE_SIZE
+    if value.size > limit:
+        raise ValidationError(f'O tamanho máximo do arquivo é de {limit}MB')
 
 # Create your models here.
 class User(AbstractUser):
@@ -17,7 +25,7 @@ class User(AbstractUser):
     def __str__(self):
         return self.full_name
     
-class Print(models.Model):
+class Print(models.Model):  
     STATUS_CHOICES = [
         ('pending', 'Pendente'),
         ('printed', 'Impresso'),
@@ -27,7 +35,7 @@ class Print(models.Model):
     id = models.AutoField(primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    attachment = models.FileField(max_length=254, upload_to=get_file_path)
+    attachment = models.FileField(max_length=254, upload_to=get_file_path, validators=[file_size])
     print_count = models.PositiveIntegerField(default=1)
     withdraw_date = models.DateField()
     withdraw_time = models.TimeField()
@@ -38,6 +46,10 @@ class Print(models.Model):
     
     def __str__(self):
         return self.created_by.username
+    
+    def get_status_display(self):
+        status_mapping = dict(self.STATUS_CHOICES)
+        return status_mapping.get(self.status, self.status)
     
 class Warning(models.Model):
     id = models.AutoField(primary_key=True)
